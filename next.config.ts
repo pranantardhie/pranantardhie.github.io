74c42f62e3c24f2e671708d51d4df2a22382e686
Ardhie Putra <ardhieputraprananta@gmail.com>
2025-05-02 17:39:25 +0700

    Fix config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  distDir: "out",
+  output: "export",
+  basePath: process.env.PAGES_BASE_PATH,
 };
 
 export default nextConfig;
